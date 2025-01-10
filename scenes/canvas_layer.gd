ce3e180a637816ecdeb4405d54cee65ceafa17da
juigperez <juigperez@gmail.com>
2025-01-10 13:35:26 -0300

    Modified Winning Condition and Added 2D Light

--- scenes/canvas_layer.gd
+++ scenes/canvas_layer.gd
@@ -2,5 +2,8 @@ extends CanvasLayer
 
 @onready var label = $Label
 
-func update_score(score):
-	label.text = "Coins: " + str(score)
+func update_score(score, winning_state):
+	if winning_state:
+		label.text = "YOU WON! BY A LOT!!!"
+	else:
+		label.text = "Coins: " + str(score)
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,10 +1,15 @@
 extends Node
 
 var score = 0
+var winning_state = false
+
 
 @onready var canvas_layer = %CanvasLayer
+@onready var coins = %Coins
+@onready var winning_condition = coins.get_child_count()
 
 func add_point():
 	score += 1
-	print(score)
-	canvas_layer.update_score(score)
+	if score == winning_condition:
+		winning_state = true
+	canvas_layer.update_score(score, winning_state)
